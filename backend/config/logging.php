dd481db1f663cd09c18911c75fb5de2600f7b7bf
kultosh <kulgole90@gmail.com>
2025-10-25 15:32:34 +0545

    Add log channel for clients

--- backend/config/logging.php
+++ backend/config/logging.php
@@ -127,6 +127,12 @@ return [
             'path' => storage_path('logs/laravel.log'),
         ],
 
+        'clients' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/clients.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'days' => 14,
+        ],
     ],
 
 ];
