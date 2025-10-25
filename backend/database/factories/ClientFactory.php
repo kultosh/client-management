7d40301a81955ba1af02e4e7504af5e4226887fc
kultosh <kulgole90@gmail.com>
2025-10-25 17:28:49 +0545

    Implement Client Listing Unit Test

---
+++ backend/database/factories/ClientFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Client;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Client>
+ */
+class ClientFactory extends Factory
+{
+    protected $model = Client::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'company_name' => $this->faker->company,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone_number' => $this->faker->phoneNumber,
+            'is_duplicate' => false,
+            'duplicate_group_id' => null,
+        ];
+    }
+}
