357db27152879ef03e244e99853f240bcf63f16e
kultosh <kulgole90@gmail.com>
2025-10-26 21:39:40 +0545

    Add scope for duplicate, unique and all filter type

--- backend/app/Models/Client.php
+++ backend/app/Models/Client.php
@@ -10,4 +10,13 @@ class Client extends Model
     use HasFactory;
     
     protected $fillable = ['company_name','email','phone_number','is_duplicate','duplicate_group_id'];
+
+    public function scopeFilterByType($query, $type)
+    {
+        return match($type) {
+            'duplicate' => $query->where('is_duplicate', true),
+            'unique' => $query->where('is_duplicate', false),
+            default => $query
+        };
+    }
 }
